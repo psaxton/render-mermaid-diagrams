35c36a1e024444ef2808676a8288898234364053
Philip Saxton <Philip.Saxton@verisk.com>
2024-05-30 15:09:27 -0600

    fixes copilot hallucinations

--- render-mermaid-diagrams.js
+++ render-mermaid-diagrams.js
@@ -1,25 +1,18 @@
 #!/usr/bin/env node
 
 const { exec } = require('child_process');
-const glob = require('glob');
+const { globSync } = require('glob');
 const path = require('path');
 const mermaidCli = path.join('node_modules', '.bin', 'mmdc');
 
-glob('**/*.mmd', (err, files) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+globSync('**/*.mmd').forEach(file => {
+  const command = `${mermaidCli} -t forest -i ${file} -o ${file}.png`;
+  exec(command, (err, stdout, _stderr) => {
+    if (err) {
+      console.error(`Error executing command "${command}": ${err}`);
+      return;
+    }
 
-  files.forEach(file => {
-    const command = `${mermaidCli} -t forest -i ${file} -o ${file}.png`;
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error executing command "${command}": ${err}`);
-        return;
-      }
-
-      console.log(stdout);
-    });
+    console.log(stdout);
   });
 });
